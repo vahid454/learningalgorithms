36f50bbbe5c1b13d57478d58a0625655e7bdf582
vahid454 <vahidmansuri702@gmail.com>
2023-05-24 22:57:04 +0530

    memoization eg

--- eg2.cpp
+++ eg2.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 int fibs(int n);
 
+/* this approach is very fast approach for finding fibonacci of a number */
 int fibs(int n)
 {
     vector<int> results( n );
@@ -12,6 +13,10 @@ int fibs(int n)
         results[ i ] = results[ i - 1 ] + results[ i - 2 ];
     return results[ n - 1 ] + results[ n - 2 ];
 }
+/*
+in results vector we are storing traced record so that
+we don't need to calculate it later
+*/
 int main()
 {
     printf("fibo(5) : %d\n",fibs(5));   
